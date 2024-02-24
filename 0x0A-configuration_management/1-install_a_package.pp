83b3a71e1a132a779ee861f3de6a2c73d8676e66
mahmoud malek <information.mahmoud@gmail.com>
2024-02-24 03:10:14 +0200

    ok

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,11 @@
 # installing flask from  pip3 using puppet
 
-package { 'python3-pip':
-    ensure => 'installed',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
-exec { 'install_flask':
-    command => 'pip3 install flask==2.1.0',
-    path    => ['/usr/bin', '/usr/local/bin'],
-    require => Package['python3-pip'],
-    unless  => 'pip3 show flask | grep Version | grep 2.1.0',
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
