5480d53ba8f9da63901caa03d7913fc8aa19808d
mahmoud malek <information.mahmoud@gmail.com>
2024-02-24 16:53:26 +0200

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,10 @@
 # set up your client SSH configuration file so that you can connect
 # to a server without typing a password.
 
-file { '~/.ssh/school':
-	ensure => present,
-	content => "Host *\n PasswordAuthentication no\n PubkeyAuthentication yes\n IdentityFile ~/.ssh/school\n",
+file { '~/.ssh/config':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => "Host *\n    IdentityFile /root/.ssh/school\n    PasswordAuthentication no\n    PubkeyAuthentication yes\n",
 }
