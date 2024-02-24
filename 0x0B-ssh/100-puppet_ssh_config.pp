081e1fe4993fb9c27d3b64fc2c4189234be12211
mahmoud malek <information.mahmoud@gmail.com>
2024-02-24 16:56:36 +0200

    try

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # set up your client SSH configuration file so that you can connect
 # to a server without typing a password.
 
-file { '~/.ssh/config':
+file { '/home/.ssh/config':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0600',
-    content => "Host *\n    IdentityFile /root/.ssh/school\n    PasswordAuthentication no\n    PubkeyAuthentication yes\n",
+    content => "Host *\n    IdentityFile /home/.ssh/school\n    PasswordAuthentication no\n    PubkeyAuthentication yes\n",
 }
