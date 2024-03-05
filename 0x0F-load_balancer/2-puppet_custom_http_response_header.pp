3ffece35277bc60d4dfa10490e35cb46084b90d1
mahmoud malek <information.mahmoud@gmail.com>
2024-03-05 16:34:59 +0200

    finally 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,65 +2,26 @@
 # The name of the custom HTTP header must be X-Served-By
 # The value of the custom HTTP header must be the hostname of the server Nginx is running on
 
-#variables
-$config = "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-
-	server_name _;
-	
-	add_header X-Served-By ${hostname};
-	
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-
-	error_page 404 /404.html;
-	location = /404.html {
-		root /var/www/html;
-		internal;
-	}
-}"
-
-#updating the package manager
-exec { 'apt-update':
-  command => '/usr/bin/apt-get update',
-  path    => '/usr/bin',
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-#installing the Nginx package
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['apt-update'],
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
-#configuring the Nginx server
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => "Hello World!\n",
-  require => Package['nginx'],
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
-file { '/var/www/html/404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page\n\n",
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => $config,
-  require => Package['nginx'],
-}
-
-#starting the Nginx service
-service { 'nginx':
-  ensure    => 'running',
-  enable    => 'true',
-  require   => Package['nginx'],
-  subscribe => [File['/var/www/html/index.html'], File['/var/www/html/404.html'],
-  File['/etc/nginx/sites-available/default']],
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
