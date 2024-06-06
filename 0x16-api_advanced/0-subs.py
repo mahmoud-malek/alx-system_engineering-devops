c0012cc8aa88759f6119567df08b5350e8c07d97
mahmoud malek <information.mahmoud@gmail.com>
2024-06-07 00:49:33 +0300

    add\udpate files :fire:

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux: me"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code is not 200:
+    if response.status_code != 200:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,12 +7,12 @@ import requests
 
 def top_ten(subreddit):
     """ Return the top ten hot posts on a given subreddit. """
-    url = "http://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {
         "User-Agent": "linux: me"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code is not 200:
+    if response.status_code != 200:
         print("None")
         return
     results = response.json().get("data").get("children")
