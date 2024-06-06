2a798878fd445c162d57c2d14756012f4ec7375f
mahmoud malek <information.mahmoud@gmail.com>
2024-06-07 00:02:58 +0300

    add\udpate files :fire:

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" a module that contains a function that queries the Reddit
-API and returns the number of subscribers
-(not active users, total subscribers)"""
+""" a module that contains a function that queries the Reddit API"""
+
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ a function that queries the Reddit API and returns
-                 the number of subscribers"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'linux:mahmoudmalek'}
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    return response.json().get('data').get('subscribers')
+    results = response.json().get("data")
+    return results.get("subscribers")
