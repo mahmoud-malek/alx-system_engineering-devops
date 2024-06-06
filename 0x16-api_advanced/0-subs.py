352ec717af8472e8b8f801e290217489caeeb0b3
mahmoud malek <information.mahmoud@gmail.com>
2024-06-06 23:59:08 +0300

    add\udpate files :fire:

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """ a function that queries the Reddit API and returns
          the number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'linux:mahmoudmalek'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
