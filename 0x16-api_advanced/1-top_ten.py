51aa6c692a4766fd7353a02bb264e83cd4966ccc
mahmoud malek <information.mahmoud@gmail.com>
2024-06-07 00:41:23 +0300

    add\udpate files :fire:

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" Function to query the top ten hot posts on a given Reddit subreddit. """
+
+import requests
+
+
+def top_ten(subreddit):
+    """ Return the top ten hot posts on a given subreddit. """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        "User-Agent": "linux: me"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data").get("children")
+    for post in results:
+        print(post.get("data").get("title"))
+    return
