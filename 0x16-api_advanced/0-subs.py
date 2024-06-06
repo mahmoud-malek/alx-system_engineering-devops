ec23c5eb302472841190aa544d0dd74288c15e0f
mahmoud malek <information.mahmoud@gmail.com>
2024-06-06 21:52:20 +0300

    add\udpate files :fire:

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	""" a function that queries the Reddit API and returns the number of subscribers"""
-	url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-	headers = {'User-Agent': 'Mozilla/5.0'}
-	response = requests.get(url, headers=headers)
-	if response.status_code == 200:
-			return response.json().get('data').get('subscribers')
-		return 0
+    """ a function that queries the Reddit API and returns the number of subscribers"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    return 0
